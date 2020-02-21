6364a8478b3e5767ac938e4b7369e6ce5e16bd1d
Leandro Araujo <leandroaraujodeveloper@gmail.com>
2020-02-21 16:15:39 -0300

    Updates

---
+++ precache-manifest.48bcf8e2162221281e2b182ff6f0dee2.js
@@ -0,0 +1,350 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba59d08d31fd13c6fb77fc28cc9c4fd0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e4abf73e20c10ad1d757",
+    "url": "./static/css/2.5ad6b3d6.chunk.css"
+  },
+  {
+    "revision": "31b65e84ef2c8c4f0596",
+    "url": "./static/css/main.3d23b365.chunk.css"
+  },
+  {
+    "revision": "e4abf73e20c10ad1d757",
+    "url": "./static/js/2.e37b1a48.chunk.js"
+  },
+  {
+    "revision": "bd59c132604905ab9aa28e857048da08",
+    "url": "./static/js/2.e37b1a48.chunk.js.LICENSE"
+  },
+  {
+    "revision": "31b65e84ef2c8c4f0596",
+    "url": "./static/js/main.9317d1d4.chunk.js"
+  },
+  {
+    "revision": "c08ff378e0dfdbb837c5",
+    "url": "./static/js/runtime-main.b78277df.js"
+  },
+  {
+    "revision": "555af978985002343c24e26d0ab39dde",
+    "url": "./static/media/01.555af978.jpeg"
+  },
+  {
+    "revision": "5647c6017ee577f6f5482b0d68061158",
+    "url": "./static/media/01.5647c601.png"
+  },
+  {
+    "revision": "9923b6184a0f6779821b87182f6e7c9f",
+    "url": "./static/media/01.9923b618.jpg"
+  },
+  {
+    "revision": "9835928455775f273ec9eef8fe94d0b7",
+    "url": "./static/media/02.98359284.png"
+  },
+  {
+    "revision": "b853923d483c1971cb14626dcb76e6a0",
+    "url": "./static/media/02.b853923d.jpeg"
+  },
+  {
+    "revision": "d69fd281f2ce0ac288b6e829fc9e4e5c",
+    "url": "./static/media/02.d69fd281.jpg"
+  },
+  {
+    "revision": "78a96cddf14b485fb7cc9bc3f6c1b6dc",
+    "url": "./static/media/03.78a96cdd.jpeg"
+  },
+  {
+    "revision": "e293312e5d4e15eb4132d7d8e7e69281",
+    "url": "./static/media/03.e293312e.png"
+  },
+  {
+    "revision": "84df3dba1c55c4f44c7d4efe38203287",
+    "url": "./static/media/04.84df3dba.jpeg"
+  },
+  {
+    "revision": "f1c8e596fdf83f839b81e6bb84465d01",
+    "url": "./static/media/04.f1c8e596.png"
+  },
+  {
+    "revision": "f1356aeaf2a94e1fdefd8f93b6a348dd",
+    "url": "./static/media/05.f1356aea.jpeg"
+  },
+  {
+    "revision": "2aaafc865f4fda4aed57e3b438ae1af6",
+    "url": "./static/media/06.2aaafc86.jpeg"
+  },
+  {
+    "revision": "401177a5b481ec04d9adee4052e4b453",
+    "url": "./static/media/07.401177a5.jpeg"
+  },
+  {
+    "revision": "094b3bf580772687c8cf941eb5b9283d",
+    "url": "./static/media/08.094b3bf5.jpeg"
+  },
+  {
+    "revision": "14cfdaac1ac9b2380b84d8c84db71338",
+    "url": "./static/media/09.14cfdaac.jpeg"
+  },
+  {
+    "revision": "94820d0078b48b4754bfb56fc4d0d8bb",
+    "url": "./static/media/1.94820d00.jpeg"
+  },
+  {
+    "revision": "c6d613c0434a81f9269a32a999d41f77",
+    "url": "./static/media/10.c6d613c0.jpeg"
+  },
+  {
+    "revision": "d87df3dd0ac3f774c1e2eaff2e3b148b",
+    "url": "./static/media/11.d87df3dd.jpeg"
+  },
+  {
+    "revision": "257d8aa71a5861a64c5b9cb688c6268d",
+    "url": "./static/media/12.257d8aa7.jpeg"
+  },
+  {
+    "revision": "68998c2041b1d9f21c922bc9c8ad64b7",
+    "url": "./static/media/13.68998c20.jpeg"
+  },
+  {
+    "revision": "cfda6095709e415819299cc113ad318a",
+    "url": "./static/media/13.cfda6095.jpg"
+  },
+  {
+    "revision": "fecdb59e43cb892b0467030b7aeb192c",
+    "url": "./static/media/14.fecdb59e.jpeg"
+  },
+  {
+    "revision": "6b58b830cdcdf5829daa30267a5951da",
+    "url": "./static/media/15.6b58b830.jpeg"
+  },
+  {
+    "revision": "94a94823c0033506d5355da0ea01d001",
+    "url": "./static/media/16.94a94823.jpeg"
+  },
+  {
+    "revision": "314aab33fdcb75c3e40ab47825655802",
+    "url": "./static/media/17.314aab33.jpeg"
+  },
+  {
+    "revision": "59b8dd7240b69a45071055d2ff80c599",
+    "url": "./static/media/2.59b8dd72.jpeg"
+  },
+  {
+    "revision": "57d6af9533eed68d0fbffc3d0b90f2f9",
+    "url": "./static/media/Diretoria.57d6af95.png"
+  },
+  {
+    "revision": "9bd6ff13c332e20602d11bec7d7da793",
+    "url": "./static/media/Leandro.9bd6ff13.jpg"
+  },
+  {
+    "revision": "db99bbdf4daf56c8d8f7fb57986c6853",
+    "url": "./static/media/LuanR.db99bbdf.JPEG"
+  },
+  {
+    "revision": "251b7677cc1bd58c84346f16b378df29",
+    "url": "./static/media/Victor.251b7677.jpg"
+  },
+  {
+    "revision": "730731390ac9c949a53de6c01cc10602",
+    "url": "./static/media/basquete.73073139.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "./static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "./static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "./static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "./static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "./static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "8380b4b9a0a6e0bb91575fc33637c346",
+    "url": "./static/media/camisa-futsal.8380b4b9.jpg"
+  },
+  {
+    "revision": "e3de6b987f37040921f0e03690d01a05",
+    "url": "./static/media/camisa-torcedor.e3de6b98.png"
+  },
+  {
+    "revision": "5d0f4459b43b29f022b2d6f2902b99f2",
+    "url": "./static/media/caneca.5d0f4459.jpg"
+  },
+  {
+    "revision": "31c19719146079bc404cdcf2d2ceb7b0",
+    "url": "./static/media/carteira_frente.31c19719.png"
+  },
+  {
+    "revision": "272a50175cf12fe85ba5f43d620970d2",
+    "url": "./static/media/carteira_verso.272a5017.png"
+  },
+  {
+    "revision": "b7b2050933fd7083c3011589abef2350",
+    "url": "./static/media/contato.b7b20509.PNG"
+  },
+  {
+    "revision": "3772f0c355bc472e3073bbdae2def810",
+    "url": "./static/media/copo.3772f0c3.jpg"
+  },
+  {
+    "revision": "013ef63990ee4ef3395df784328f085f",
+    "url": "./static/media/cs.013ef639.jpg"
+  },
+  {
+    "revision": "4d88b739706c8bf0a18ed988299fee2e",
+    "url": "./static/media/cs.4d88b739.jpeg"
+  },
+  {
+    "revision": "5a8cbeb87f8bbeedcc8f5dc44d655a37",
+    "url": "./static/media/e-sports.5a8cbeb8.jpeg"
+  },
+  {
+    "revision": "5767897ca420e3a337ffd64fc18cb75d",
+    "url": "./static/media/escudo.5767897c.png"
+  },
+  {
+    "revision": "4925baff567e4cfd0fc53ec0d8231d7c",
+    "url": "./static/media/express.4925baff.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "./static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "4b8f824f2a8b8b3204cf1e3b8dbda5e2",
+    "url": "./static/media/free.4b8f824f.jpg"
+  },
+  {
+    "revision": "1efbc50d32e9c5b938e2effa5758f439",
+    "url": "./static/media/futsal.1efbc50d.jpg"
+  },
+  {
+    "revision": "d29df5fb1dc3bae3dfa108ad2ae8a9e1",
+    "url": "./static/media/futsal.d29df5fb.jpeg"
+  },
+  {
+    "revision": "38b43fa1dd096a178affcecba7ec5929",
+    "url": "./static/media/handebol.38b43fa1.jpeg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "./static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "./static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "./static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "./static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "./static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "d4645ee42ab79598d9d1602f0b5d5fd2",
+    "url": "./static/media/instagram.d4645ee4.png"
+  },
+  {
+    "revision": "c44a5016a6ffb4cc0145616b3d1a0eb7",
+    "url": "./static/media/judo.c44a5016.jpeg"
+  },
+  {
+    "revision": "8b0db6ff2c5e052e0308fa30978ece6d",
+    "url": "./static/media/lattes.8b0db6ff.png"
+  },
+  {
+    "revision": "564a2c3aa7bd15d809e2c8915eb88368",
+    "url": "./static/media/league.564a2c3a.jpg"
+  },
+  {
+    "revision": "5dc4bb54e7463d0c255a84e12898a9ed",
+    "url": "./static/media/linkedin.5dc4bb54.png"
+  },
+  {
+    "revision": "f23848277fbc08bcd1e559f88965f2e3",
+    "url": "./static/media/logo-ej.f2384827.png"
+  },
+  {
+    "revision": "14af7ad11b8837d1e2b26339d72442fc",
+    "url": "./static/media/logo.14af7ad1.png"
+  },
+  {
+    "revision": "741db962147b7c6fa1572a5d578e254d",
+    "url": "./static/media/muaythai.741db962.jpg"
+  },
+  {
+    "revision": "6aa93b68eb02879babad5d348406db11",
+    "url": "./static/media/nodejs.6aa93b68.jpg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "./static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "./static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "./static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "./static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "./static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "144b80220c608e2a923639e730527b0a",
+    "url": "./static/media/parceria1.144b8022.jpg"
+  },
+  {
+    "revision": "72ce92666bada012b6f2aa62515cba55",
+    "url": "./static/media/parceria2.72ce9266.jpg"
+  },
+  {
+    "revision": "549548a0de63e167bbf96a801baa4e0f",
+    "url": "./static/media/postgres.549548a0.png"
+  },
+  {
+    "revision": "580ffd26ccaa16481ed87c1829c74943",
+    "url": "./static/media/react.580ffd26.png"
+  },
+  {
+    "revision": "3162d43e9a0f52db7e5bedc6e7751c26",
+    "url": "./static/media/sinuca.3162d43e.jpg"
+  },
+  {
+    "revision": "e5cd991fe785939b36e01a025fd11544",
+    "url": "./static/media/truco.e5cd991f.jpg"
+  },
+  {
+    "revision": "9849a1736a96b2ecc36caa65fd2116fb",
+    "url": "./static/media/volei.9849a173.jpg"
+  },
+  {
+    "revision": "3b1b804548578e143651121b74e2685a",
+    "url": "./static/media/xadrez.3b1b8045.jpg"
+  }
+]);
